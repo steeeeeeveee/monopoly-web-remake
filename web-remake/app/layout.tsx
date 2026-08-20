e51c778308963736c82d36722bfba9053666ddf1
Steeeeeveee <301966674+Steeeeeveee@users.noreply.github.com>
2026-08-21 01:23:26 +0800

    chore: add site sharing metadata

--- web-remake/app/layout.tsx
+++ web-remake/app/layout.tsx
@@ -2,8 +2,29 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://dafuweng-steve.steve2329362568.chatgpt.site'),
   title: '大富翁网页版',
   description: '支持电脑和手机浏览器的多人同屏与人机对战大富翁游戏。',
+  openGraph: {
+    title: '大富翁网页版',
+    description: '支持电脑和手机浏览器的多人同屏与人机对战大富翁游戏。',
+    type: 'website',
+    locale: 'zh_CN',
+    images: [
+      {
+        url: '/og.png',
+        width: 1200,
+        height: 630,
+        alt: '大富翁网页版',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: '大富翁网页版',
+    description: '支持电脑和手机浏览器的多人同屏与人机对战大富翁游戏。',
+    images: ['/og.png'],
+  },
   icons: {
     icon: '/favicon.svg',
     shortcut: '/favicon.svg',
@@ -19,4 +40,3 @@ export default function RootLayout({
     </html>
   )
 }
-
